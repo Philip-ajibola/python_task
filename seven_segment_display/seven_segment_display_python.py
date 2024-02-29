f40d33d1a4d18963eaaf822d78f4b65ca204b6a8
Philip <ajibolaphilip10@gmail.com>
2024-02-28 21:45:43 -0800

    commit

--- seven_segment_display/seven_segment_display_python.py
+++ seven_segment_display/seven_segment_display_python.py
@@ -1,7 +1,19 @@
 def display_seven_segment_of(binary_number):
-    list_char =[]
+    list_char = []
     list_char += binary_number
-    print(list_char)
+    bool_list = []
+    for character in list_char:
+        bool_list.append(character == "1")
+
+
+def display_horizontal(condition):
+    if condition: print(" # # # # #")
+
+
+def display_vertical(condition1, condition2):
+    if condition1 and condition2: print(" #         #")
+    if condition1 is False and condition2 is True: print("         #")
+    if condition1 is True and condition2 is False: print("         #")
 
 
 number = "11111111"
