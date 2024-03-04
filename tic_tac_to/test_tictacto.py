c69728b240ee58f5dc52df0c3f8161b1db4171d7
Philip <ajibolaphilip10@gmail.com>
2024-03-04 00:51:53 -0800

    commit

--- tic_tac_to/test_tictacto.py
+++ tic_tac_to/test_tictacto.py
@@ -7,6 +7,7 @@ from tic_tac_to.value import Value
 class TestTicTacTo(TestCase):
     def setUp(self):
         self.ticTacto = TicTacTo()
+        self.players = self.ticTacto.get_player()
 
     def test_that_game_has_a_marking_board(self):
         expected = self.ticTacto.get_board()
@@ -15,26 +16,51 @@ class TestTicTacTo(TestCase):
         self.assertEqual(expected, board)
 
     def test_that_game_has_two_players(self):
-        players = self.ticTacto.get_player()
-        self.assertEqual(2, len(players))
+        self.assertEqual(2, len(self.players))
 
     def test_that_player_can_play_game_and_fill_board(self):
-        players = self.ticTacto.get_player()
-        players[0].play(1, self.ticTacto)
-        self.assertEqual(Value.X, players[0].get_value())
+        self.players[0].play(1, self.ticTacto)
+        self.assertEqual(Value.X, self.players[0].get_value())
 
     def test_that_player_one_has_a_unique_value(self):
-        players = self.ticTacto.get_player()
-        self.assertEqual(Value.X, players[0].get_value())
+        self.assertEqual(Value.X, self.players[0].get_value())
 
     def test_that_player_two_has_a_unique_value(self):
-        players = self.ticTacto.get_player()
-        self.assertEqual(Value.O, players[1].get_value())
+        self.assertEqual(Value.O, self.players[1].get_value())
 
     def test_that_player_one_has_a_unique_id(self):
-        players = self.ticTacto.get_player()
-        self.assertEqual(1, players[0].get_id())
+        self.assertEqual(1, self.players[0].get_id())
 
     def test_that_player_two_has_a_unique_id(self):
-        players = self.ticTacto.get_player()
-        self.assertEqual(2, players[1].get_id())
+        self.assertEqual(2, self.players[1].get_id())
+
+    def test_that_row_one_can_be_filled(self):
+        self.players[0].play(1, self.ticTacto)
+        self.players[1].play(2, self.ticTacto)
+        self.players[0].play(3, self.ticTacto)
+        expected = [Value.X, Value.O, Value.X]
+        self.assertEqual(expected, self.ticTacto.get_board()[0])
+
+    def test_that_row_two_can_be_filled(self):
+        self.players[0].play(4, self.ticTacto)
+        self.players[1].play(5, self.ticTacto)
+        self.players[0].play(6, self.ticTacto)
+        expected = [Value.X, Value.O, Value.X]
+        self.assertEqual(expected, self.ticTacto.get_board()[1])
+
+    def test_that_row_three_can_be_filled(self):
+        self.players[0].play(7, self.ticTacto)
+        self.players[1].play(8, self.ticTacto)
+        self.players[0].play(9, self.ticTacto)
+        expected = [Value.X, Value.O, Value.X]
+        self.assertEqual(expected, self.ticTacto.get_board()[2])
+
+    def test_that_when_number_greater_than_9_is_entered_error_message_is_thrown(self):
+        self.players[0].play(7, self.ticTacto)
+        self.players[1].play(8, self.ticTacto)
+        with self.assertRaises(ValueError):
+            self.players[0].play(7, self.ticTacto)
+
+    def test_that_when_Player_try_to_play_in_an_already_filled_box_error_is_thrown(self):
+        with self.assertRaises(ValueError):
+            self.players[0].play(10, self.ticTacto)
--- tic_tac_to/tictacto.py
+++ tic_tac_to/tictacto.py
@@ -8,7 +8,8 @@ class TicTacTo:
     def __init__(self):
         value = Value.EMPTY
         self.board = [[value, value, value], [value, value, value], [value, value, value]]
-        self.players =[]
+        self.players = []
+        self.box_checker = {}
         player1 = Player(1, Value.X)
         player2 = Player(2, Value.O)
         self.players.append(player1)
@@ -20,11 +21,15 @@ class TicTacTo:
     def get_player(self):
         return self.players
 
-    def pickPosition(self, position, value):
+    def pickPosition(self, position: int, value: Value):
+        self.validate_user_input(position)
+        self.check_if_box_is_empty(position)
         self.check_if_player_played_in_row_one_and_fill_board(position, value)
+        self.check_if_player_played_in_row_two_and_fill_board(position, value)
+        self.check_if_player_played_in_row_three_and_fill_board(position, value)
 
-    def check_if_player_played_in_row_one_and_fill_board(self, position, value):
-        if 0 > position <= 3:
+    def check_if_player_played_in_row_one_and_fill_board(self, position: int, value: Value):
+        if 0 < position <= 3:
             if position == 1:
                 self.board[0][0] = value
             elif position == 2:
@@ -32,17 +37,17 @@ class TicTacTo:
             else:
                 self.board[0][2] = value
 
-    def check_if_player_played_in_row_two_and_fill_board(self, position, value):
-        if 3 > position <= 6:
-            if position == 1:
+    def check_if_player_played_in_row_two_and_fill_board(self, position: int, value: Value):
+        if 3 < position <= 6:
+            if position == 4:
                 self.board[1][0] = value
-            elif position == 2:
+            elif position == 5:
                 self.board[1][1] = value
             else:
                 self.board[1][2] = value
 
-    def check_if_player_played_in_row_three_and_fill_board(self, position, value):
-        if 6 > position <= 9:
+    def check_if_player_played_in_row_three_and_fill_board(self, position: int, value: Value):
+        if 6 < position <= 9:
             if position == 7:
                 self.board[2][0] = value
             elif position == 8:
@@ -50,3 +55,10 @@ class TicTacTo:
             else:
                 self.board[2][2] = value
 
+    def validate_user_input(self, user_input: int):
+        if user_input > 9: raise ValueError("Box Number not found")
+
+    def check_if_box_is_empty(self, position: int):
+        condition = self.box_checker.add(position)
+        if not condition:
+            raise ValueError("Box not empty")
