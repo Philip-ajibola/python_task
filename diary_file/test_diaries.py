d46578d52c144ea7ea2f02bc81d745fd1fc81997
Philip <ajibolaphilip10@gmail.com>
2024-03-04 16:56:47 -0800

    commit

--- diary_file/test_diaries.py
+++ diary_file/test_diaries.py
@@ -4,34 +4,32 @@ from diary_file.diaries import Diaries
 
 
 class TestDiaries(TestCase):
+    def setUp(self):
+        self.diaries = Diaries()
+
     def test_that_I_can_add_to_Diaries(self):
-        diaries = Diaries()
-        diaries.add("user_name", "password")
-        self.assertEqual(1, len(diaries.get_list_of_diary()))
+        self.diaries.add("user_name", "password")
+        self.assertEqual(1, len(self.diaries.get_list_of_diary()))
 
     def test_that_i_find_diary_by_user_name(self):
-        diaries = Diaries()
-        diaries.add("user_name", "password")
-        entry = diaries.find_by_user_name("user_name")
-        self.assertEqual(entry, diaries.get_list_of_diary()[0])
+        self.diaries.add("user_name", "password")
+        entry = self.diaries.find_by_user_name("user_name")
+        self.assertEqual(entry, self.diaries.get_list_of_diary()[0])
 
     def test_that_if_wrong_user_name_is_entered_to_find_diary_error_is_thrown(self):
-        diaries = Diaries()
-        diaries.add("user_name", "password")
+        self.diaries.add("user_name", "password")
         with self.assertRaises(ValueError):
-            diaries.find_by_user_name("my_user")
+            self.diaries.find_by_user_name("my_user")
 
     def test_that_I_can_delete_diary(self):
-        diaries = Diaries()
-        diaries.add("user_name", "password")
-        diaries.delete("user_name", "password")
-        self.assertEqual(0, len(diaries.get_list_of_diary()))
+        self.diaries.add("user_name", "password")
+        self.diaries.delete("user_name", "password")
+        self.assertEqual(0, len(self.diaries.get_list_of_diary()))
 
     def test_that_diary_cant_be_deleted_if_the_password_is_wrong(self):
-        diaries = Diaries()
-        diaries.add("user_name", "password")
+        self.diaries.add("user_name", "password")
         with self.assertRaises(ValueError):
-            diaries.delete("user_name", "wrong_password")
+            self.diaries.delete("user_name", "wrong_password")
 
     def test_that_diary_cant_be_deleted_if_the_username_is_wrong(self):
         diaries = Diaries()
