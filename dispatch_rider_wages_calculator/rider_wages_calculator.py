420ff2b851e0660c44da016b9e2aca11879d2a4e
Philip <ajibolaphilip10@gmail.com>
2024-03-07 00:20:07 -0800

    commit

--- dispatch_rider_wages_calculator/rider_wages_calculator.py
+++ dispatch_rider_wages_calculator/rider_wages_calculator.py
@@ -1,1 +1,5 @@
-def calculate_rider_wages(number_of_delivery: int)-> int:
+def calculate_rider_wages(number_of_delivery: int) -> int:
+    if number_of_delivery < 50:
+        return (number_of_delivery * 160) + 5_000
+    if 50 < number_of_delivery < 60:
+        return (number_of_delivery * 200) + 5_000
--- dispatch_rider_wages_calculator/test_rider_wages_calculator.py
+++ dispatch_rider_wages_calculator/test_rider_wages_calculator.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
+import rider_wages_calculator
 
 
 class Test(TestCase):
-    def test_calculate_rider_wages(self):
-        self.fail()
+    def test_when_rider_delivery_is_less_than_50_number_of_delivery_is_multiplied_by_160(self):
+        expectedWages = rider_wages_calculator.calculate_rider_wages(25)
+        self.assertEqual(expectedWages, 9_000)
+
+    def test_when_rider_delivery_is_at_theRange_of_50_59_number_of_delivery_is_multiplied_by_160(self):
+        expectedWages = rider_wages_calculator.calculate_rider_wages(57)
+        self.assertEqual(expectedWages, 16_400)
