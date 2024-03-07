e5c3d462d0cc1f61e925e2f73995e8d720309f39
Philip <ajibolaphilip10@gmail.com>
2024-03-07 00:30:40 -0800

    commit

--- dispatch_rider_wages_calculator/rider_wages_calculator.py
+++ dispatch_rider_wages_calculator/rider_wages_calculator.py
@@ -3,5 +3,7 @@ def calculate_rider_wages(number_of_delivery: int) -> int:
         return (number_of_delivery * 160) + 5_000
     if 50 < number_of_delivery < 60:
         return (number_of_delivery * 200) + 5_000
-    if 50 < number_of_delivery < 60:
+    if 60 < number_of_delivery < 70:
         return (number_of_delivery * 250) + 5_000
+    else:
+        return (number_of_delivery * 500) + 5_000
--- dispatch_rider_wages_calculator/test_rider_wages_calculator.py
+++ dispatch_rider_wages_calculator/test_rider_wages_calculator.py
@@ -14,3 +14,7 @@ class Test(TestCase):
     def test_when_rider_delivery_is_at_theRange_of_60_69_number_of_delivery_is_multiplied_by_160(self):
         expectedWages = rider_wages_calculator.calculate_rider_wages(69)
         self.assertEqual(expectedWages, 22_250)
+
+    def test_when_rider_delivery_is_greater_than_or_equal_to_70_number_of_delivery_is_multiplied_by_160(self):
+        expectedWages = rider_wages_calculator.calculate_rider_wages(80)
+        self.assertEqual(expectedWages, 45_000)
